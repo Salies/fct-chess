5e94e1704a55b000596a1d429d82b714fa0ae393
Daniel Serezane <serezane@protonmail.com>
2021-07-23 15:51:18 -0300

    Peças e início Zobrist

--- temp/zobrist.cpp
+++ temp/zobrist.cpp
@@ -1,33 +1,37 @@
 #include <iostream>
-#include <map>
+#include <string>
+#include <random>
+//#include <bits/stdc++.h>
 
-typedef struct piece
-{
-    char type;
-    bool black;
-} Piece;
+char piece_list[6] = {'K', 'Q', 'R', 'B', 'N', 'P'};
 
-std::map<char, short> piecelist = {
-    {'K', 0},
-    {'Q', 1},
-    {'R', 2},
-    {'B', 3},
-    {'N', 4},
-    {'P', 5}
-};
+std::string get_piece_code(unsigned short p){
+    if(p > 6) 
+        return "B" + std::string(1, piece_list[p - 7]);
+    return "W" + std::string(1, piece_list[p - 1]);
+}
 
-short get_piece_code(Piece p){
-    if(p.black)
-        return 6 + piecelist[p.type];
-    return piecelist[p.type];
+uint64_t z_table[8][8][12];
+std::mt19937 mt_rand(6661); // wapOqjQ4r_M
+
+uint64_t rnd_bitstr(){
+    std::uniform_int_distribution<uint64_t> dist(0, UINT64_MAX);
+    return dist(mt_rand);
+}
+
+void init_zobrist(){
+    for(int i= 0; i < 8; i++)
+        for(int j = 0; j < 8; j++)
+            for(int k = 0; k < 12; k++)
+                z_table[i][j][k] = rnd_bitstr();
 }
 
 int main(void){
-    Piece p1;
-    p1.type = 'K';
-    p1.black = false;
+    std::cout << get_piece_code(7) << "\n";
 
-    std::cout << get_piece_code(p1);
+    init_zobrist();
+
+    std::cout << z_table[0][0][0];
 
     return 0;
 }
