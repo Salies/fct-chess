3efb55be855b9e1743b2078c66dd75b8f0f68c90
Daniel Serezane <serezane@protonmail.com>
2021-07-23 23:39:27 -0300

    hash

--- temp/zobrist.cpp
+++ temp/zobrist.cpp
@@ -5,13 +5,35 @@
 
 char piece_list[6] = {'K', 'Q', 'R', 'B', 'N', 'P'};
 
+/*
+0/6 - reis
+1/7 - damas
+2/8 - torres
+3/8 - bispos
+4/10 - cavalos
+5/11 - peões
+6/12 - reis que podem rocar => não implementado
+7/13 - peões que podem capturar en passant => não implementado
+*/
+
+int board[8][8] = {
+    {-1, -1, -1, 6, -1, -1, -1, -1},
+    {-1, 8, -1, -1, -1, -1, 7, -1},
+    {-1, -1, -1, -1, -1, -1, -1, -1},
+    {-1, 11, -1, -1, -1, -1, 5, -1},
+    {-1, -1, -1, -1, -1, 5, -1, -1},
+    {-1, -1, -1, -1, -1, -1, -1, -1},
+    {5, -1, -1, -1, 3, -1, -1, 1},
+    {-1, -1, -1, -1, 4, -1, -1, 0}
+};
+
 std::string get_piece_code(unsigned short p){
     if(p > 6) 
         return "B" + std::string(1, piece_list[p - 7]);
     return "W" + std::string(1, piece_list[p - 1]);
 }
 
-uint64_t z_table[8][8][12];
+uint64_t z_table[8][8][16];
 std::mt19937 mt_rand(6661); // wapOqjQ4r_M
 
 uint64_t rnd_bitstr(){
@@ -22,8 +44,18 @@ uint64_t rnd_bitstr(){
 void init_zobrist(){
     for(int i= 0; i < 8; i++)
         for(int j = 0; j < 8; j++)
-            for(int k = 0; k < 12; k++)
+            for(int k = 0; k < 16; k++)
                 z_table[i][j][k] = rnd_bitstr();
+}
+
+uint64_t hash(int b[8][8]){
+    uint64_t h = 0;
+    for(int i = 0; i < 8; i++)
+        for(int j = 0; j < 8; j++)
+            if(b[i][j] != -1)
+                h ^= z_table[i][j][b[i][j]];
+
+    return h;
 }
 
 int main(void){
@@ -31,7 +63,7 @@ int main(void){
 
     init_zobrist();
 
-    std::cout << z_table[0][0][0];
+    std::cout << hash(board);
 
     return 0;
 }
